4270bf9ff053b1ad85ab018d1aa4493e3227f1f1
woollim <137277221+woollim23@users.noreply.github.com>
2024-11-08 00:09:26 +0900

    [Complex] 에셋추가, 맵꾸미기, 리드미 수정

--- Unity_SteadyPractice_Week4/Assets/Scripts/Managers/CharacterManager.cs
+++ Unity_SteadyPractice_Week4/Assets/Scripts/Managers/CharacterManager.cs
@@ -2,5 +2,10 @@
 
 public class CharacterManager : Singletone<CharacterManager>
 {
-
+    public Player _player;
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
 }
---
+++ Unity_SteadyPractice_Week4/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpPower;
+    private Vector2 curMovementInput;
+    public LayerMask groundLayerMask;
+
+    private float bottomOffset;
+
+
+    [Header("Look")]
+    public Transform cameraContainer; // 카메라 방향을 지정할 트랜스폼
+    public float minXLook; // 회전 범위 최저값
+    public float maxXLook; // 회전 범위 최대값
+    private float camCurXRot; // 최종 출력 마우스 델타값
+    public float lookSensitivity; // 회전 민감도
+    private Vector2 mouseDelta; // inputsystem으로 입력 받는 마우스 델타값
+    public bool canLook = true;
+    public bool isInputBlocked = false; // 입력을 막는 플래그
+
+    [Header("event")]
+    public Action inventory;
+    public event Action<bool> onMoveEvent; // 이동 애니 이벤트
+    public event Action onJumpEvent; // 점프 애니 이벤트
+
+    private Rigidbody _rigidbody;
+    public CapsuleCollider _capsuleCollider;
+}
